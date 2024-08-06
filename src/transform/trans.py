69e061ed90c086f30141d959f5f518f7ddd29075
DONGUK777 <kimdongyork@gmail.com>
2024-08-06 11:03:09 +0900

    add trans.py

---
+++ src/transform/trans.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def apply_type2df(load_dt='', path="~/data/2019movie"):
+    df = pd.read_parquet(f'{path}/{load_dt}')
+    df['rnum'] = pd.to_numeric(df['rnum'])
+    assert str(df['rnum'].dtype) in ['int64']
+    num_cols = ['rnum', 'audiAcc']
+
+    df[num_cols] = df[num_cols].apply(pd.to_numeric)
+    return df
---
+++ tests/test_call.py
@@ -0,0 +1,14 @@
+from src.transform.trans import apply_type2df
+import pandas as pd
+
+def test_apply_type2df():
+    df = apply_type2df()
+    assert isinstance(df, pd.DataFrame)
+    assert str(df['rnum'].dtype) in ['int64']
+    assert str(df['audiAcc'].dtype) in ['int64']
+    
+    num_cols = ['rnum', 'audiAcc']
+
+    for c in num_cols:
+        assert df[c].dtype in ['int64', 'float64']
+    print(df)
