c86d00f2613e343c8595034006c86098a3751d56
thephunkmonk <andrewchoi1218@gmail.com>
2024-08-06 14:37:26 +0900

    added cols

--- src/transform/trans.py
+++ src/transform/trans.py
@@ -4,7 +4,7 @@ def apply_type2df(load_dt='20190101', path="~/data/2019movie"):
     df = pd.read_parquet(f'{path}/{load_dt}.parquet')
     df['rnum'] = pd.to_numeric(df['rnum'])
     assert str(df['rnum'].dtype) in ['int64']
-    num_cols = ['rnum', 'audiAcc']
+    num_cols = ['salesAmt', 'saleShare', 'salesInten', 'salesChange', 'salesAcc', "audiCnt","audiInten","audiChange","audiAcc","scrnCnt","showCnt","load_dt"] 
 
     df[num_cols] = df[num_cols].apply(pd.to_numeric)
     return df
