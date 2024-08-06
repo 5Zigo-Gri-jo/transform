b97aaefae8e87338241f42ce4b0fea540024965a
thephunkmonk <andrewchoi1218@gmail.com>
2024-08-06 12:28:57 +0900

    eds

--- src/transform/trans.py
+++ src/transform/trans.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def apply_type2df(load_dt='20190101', path="~/data/2019movie"):
-    df = pd.read_parquet(f'{path}/{load_dt}')
+    df = pd.read_parquet(f'{path}/{load_dt}.parquet')
     df['rnum'] = pd.to_numeric(df['rnum'])
     assert str(df['rnum'].dtype) in ['int64']
     num_cols = ['rnum', 'audiAcc']
